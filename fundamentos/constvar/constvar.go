0287b423cbe51c4d7e3715968f44f724ea727369
eu-larissasouza <larissa.alves.souza@outlook.com>
2025-06-05 14:47:23 -0300

    demonstrating constants and variables in Go

--- fundamentos/constvar/constvar.go
+++ fundamentos/constvar/constvar.go
@@ -1,12 +1,13 @@
 package main
 
 /*
-	AULA 1 - Constantes e Variáveis em Go
+	AULA - CONSTANTES E VARIÁVEIS EM GO
 */
 
 import (
 	"fmt"
-	"math"
+	m "math" // m - alias para o pacote math
+	// _ "math" - Importaria o pacote math sem usá-lo diretamente
 )
 
 func main() {
@@ -17,8 +18,29 @@ func main() {
 
 	// Forma reduzida de declarar variáveis
 	// Declaramos a variável area e atribuimos o valor
-	// Uma variavel declarada e não usada gera um erro de compilação
-	area := pi * math.Pow(raio, 2)
+	// Uma variável declarada e não usada gera um erro de compilação
+	area := pi * m.Pow(raio, 2)
 
 	fmt.Println("A área da circunferência é:", area)
+
+	// Declaração de constantes e variáveis em blocos
+	const (
+		a = 1
+		b = 2
+	)
+
+	var (
+		c = 3
+		d = 4
+	)
+
+	fmt.Println(a, b, c, d)
+
+	// Declarando multiplas variáveis
+	var e, f bool = true, false
+	fmt.Println(e, f)
+
+	// Declarando multiplas variáveis com inferência de tipo
+	g, h, i := 2, false, "opa"
+	fmt.Println(g, h, i)
 }
